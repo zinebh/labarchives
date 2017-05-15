e180485021ca5d230b9965ce9354001bcdf5c11b
zinebh <zineb.hassani@umassmed.edu>
2017-05-15 10:40:34 -0400

    Update FreezerBox_V2.js

--- FreezerBox_V2.js
+++ FreezerBox_V2.js
@@ -1,5 +1,5 @@
 /*
-Author: Eileen Campbell; Modified by Zineb Hassani
+Author: Eileen Campbell
 */
 (function($) {
   
@@ -17,9 +17,9 @@ Author: Eileen Campbell; Modified by Zineb Hassani
         size : '9x9', // string; format '#x#'
         color : '(None)',   // string; values are: "(None)","black","blue","brown","gray","green","natural","orange","pink","purple","red","white","yellow"
         //loc : '',     // string; location
-        box : '',
-        shelf: '',
-        rack: '',
+        box : '', //String box
+        shelf: '', //String shelf
+        rack: '', //String rack
         desc : '',    // string; description,
         col_labels: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'], // array: column label values,
         row_labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], // array: row label values,
