8e439eac7b4521d8e7fc19f5c4b2c80ad8681468
zinebh <zineb.hassani@umassmed.edu>
2017-07-10 13:21:43 -0400

    Update jquery.FreezerBox2.js

--- jquery.FreezerBox2.js
+++ jquery.FreezerBox2.js
@@ -21,7 +21,7 @@ Author: Eileen Campbell
         shelf: '',
         rack: '',
         desc : '',    // string; description,
-        col_labels: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'], // array: column label values,
+        col_labels: ['AAAA','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'], // array: column label values,
         row_labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], // array: row label values,
         cell_data : {}, // map; cell: cell form
         json_str : '',  // if json_str is not empty, it overrides all ui-freezerbox-info and ui-freezerbox-grid values
